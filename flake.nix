a4170cf5ce64ace1a078f5261a645e072c95c25c
Sizhe Zhao <prc.zhao@outlook.com>
2025-03-08 22:16:35 +0800

    feat(flake): add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ flake-parts, treefmt-nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
     };
 }
