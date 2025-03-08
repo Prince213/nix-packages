4db3c95526dec2ed647eeae7e3c928790d054dba
Sizhe Zhao <prc.zhao@outlook.com>
2025-03-08 23:38:53 +0800

    feat: add license

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
         treefmt = {
           projectRootFile = "flake.nix";
           settings.global.excludes = [
+            "LICENSE"
             "README.md"
           ];
           programs.nixfmt.enable = true;
