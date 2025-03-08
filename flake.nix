6397a5240c0c171f9a45024f4ea4b5845d45dfaf
Sizhe Zhao <prc.zhao@outlook.com>
2025-03-08 21:56:01 +0800

    feat(flake): init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+    };
+}
