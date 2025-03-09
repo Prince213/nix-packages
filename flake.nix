2c4f5ab8b955ef0e39d9f509928355b3ebfe04fb
Sizhe Zhao <prc.zhao@outlook.com>
2025-03-09 15:14:39 +0800

    feat(flake): add overlay

--- flake.nix
+++ flake.nix
@@ -9,20 +9,40 @@
   };
 
   outputs =
-    inputs@{ flake-parts, treefmt-nix, ... }:
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      treefmt-nix,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake = {
         modulePackages.ngbe = ./pkgs/ngbe;
+
+        overlays = {
+          default = self: super: {
+            firewalld = super.callPackage ./pkgs/firewalld { };
+          };
+          firewalld = self: super: {
+            firewalld = super.callPackage ./pkgs/firewalld { };
+          };
+        };
       };
       systems = [ "x86_64-linux" ];
       imports = [
         treefmt-nix.flakeModule
       ];
       perSystem =
-        { pkgs, ... }:
+        { system, pkgs, ... }:
         {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+
           packages = {
-            firewalld = pkgs.callPackage ./pkgs/firewalld { };
+            inherit (pkgs) firewalld;
           };
           treefmt = {
             projectRootFile = "flake.nix";
