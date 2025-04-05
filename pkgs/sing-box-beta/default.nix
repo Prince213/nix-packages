09e6c3b802d613edba4484e473627f1429e2109c
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-05 13:19:22 +0800

    feat(sing-box-beta): 1.12.0-alpha.23 -> 1.12.0-beta.1

--- pkgs/sing-box-beta/default.nix
+++ pkgs/sing-box-beta/default.nix
@@ -2,12 +2,12 @@
 
 sing-box.overrideAttrs (previousAttrs: rec {
   pname = previousAttrs.pname + "-beta";
-  version = "1.12.0-alpha.23";
+  version = "1.12.0-beta.1";
   src = previousAttrs.src.override {
     rev = "v${version}";
-    hash = "sha256-3Fe7XuO9NVR6Ry/a+jERE8fcz+glbOxHUpU8ApZJrUc=";
+    hash = "sha256-I6fFt1GZsrHl302wbxwKGyBbwvAgUA9cEpxnLWdN5Vs=";
   };
-  vendorHash = "sha256-s3kHLfOsYqJW4rQsNHbd2mWjvrrA0w/CbtjaaqiYoGw=";
+  vendorHash = "sha256-fiNv3hdasa/t/v0F5Tsifnk0unMv/p0cudE3sSF6rLs=";
   ldflags = [
     "-X=github.com/sagernet/sing-box/constant.Version=${version}"
   ];
