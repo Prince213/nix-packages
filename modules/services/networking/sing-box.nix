d17068c515e84ae2176794176b5aad0dbe2bc40b
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-22 14:20:22 +0800

    modules/sing-box: configure user and group for systemd service

--- modules/services/networking/sing-box.nix
+++ modules/services/networking/sing-box.nix
@@ -5,6 +5,38 @@ let
 in
 
 {
+  options.services.sing-box = {
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "sing-box";
+      description = ''
+        User that sing-box runs as.
+      '';
+    };
+
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = "sing-box";
+      description = ''
+        Group that sing-box runs as.
+      '';
+    };
+  };
+
   config = lib.mkIf cfg.enable {
+    systemd.services.sing-box = {
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+      };
+    };
+
+    users = {
+      users.${cfg.user} = {
+        isSystemUser = true;
+        inherit (cfg) group;
+      };
+      groups.${cfg.group} = { };
+    };
   };
 }
