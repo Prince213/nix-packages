d78c80b5bb12274364714a2e92f8b03722676a70
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-24 14:58:18 +0800

    flake: use nixpkgs/nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
