9b3db49a3070e50da88c4ec6a596d0f8511b71a7
Sizhe Zhao <prc.zhao@outlook.com>
2025-09-13 16:39:04 +0800

    pkgs/sing-box-beta: 1.12.0-rc.3 -> 1.13.0-alpha.8

--- pkgs/sing-box-beta/default.nix
+++ pkgs/sing-box-beta/default.nix
@@ -4,28 +4,11 @@
 
 sing-box.overrideAttrs (previousAttrs: {
   pname = previousAttrs.pname + "-beta";
-  version = "1.12.0-rc.3";
+  version = "1.13.0-alpha.8";
 
   src = previousAttrs.src.override {
-    hash = "sha256-qkrBVd84a6/z2p/lHCgrRqfualI25MAaNeYYv7v+1Xw=";
+    hash = "sha256-4LtLeh+1fiCAwgHZK48YMHJOivFj3OGJUpWdlpK7PFE=";
   };
 
-  vendorHash = "sha256-tyGCkVWfCp7F6NDw/AlJTglzNC/jTMgrL8q9Au6Jqec=";
-
-  tags = [
-    "with_quic"
-    "with_dhcp"
-    "with_wireguard"
-    "with_utls"
-    "with_acme"
-    "with_clash_api"
-    "with_gvisor"
-    "with_tailscale"
-  ];
-
-  postInstall = previousAttrs.postInstall + ''
-    install -Dm444 release/config/sing-box.sysusers $out/lib/sysusers.d/sing-box.conf
-    install -Dm444 release/config/sing-box.rules $out/share/polkit-1/rules.d/sing-box.rules
-    install -Dm444 release/config/sing-box-split-dns.xml $out/share/dbus-1/system.d/sing-box-split-dns.conf
-  '';
+  vendorHash = "sha256-vzjM7jOiOGyY+ucDABzWWo5a+W1+msnHsdMxHi7yb1A=";
 })
