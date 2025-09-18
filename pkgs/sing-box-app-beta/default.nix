f3070ed5ec5be4a06f3b97ba814de40e4ac0be46
Sizhe Zhao <prc.zhao@outlook.com>
2025-09-18 21:23:24 +0800

    pkgs/sing-box-app-beta: 1.13.0-alpha.15 -> 1.13.0-alpha.16

--- pkgs/sing-box-app-beta/default.nix
+++ pkgs/sing-box-app-beta/default.nix
@@ -8,11 +8,11 @@
 (sing-box-app.override { sing-box = sing-box-beta; }).overrideAttrs (
   finalAttrs: previousAttrs: {
     pname = previousAttrs.pname + "-beta";
-    version = "1.13.0-alpha.15";
+    version = "1.13.0-alpha.16";
 
     src = fetchzip {
       url = "https://github.com/SagerNet/sing-box/releases/download/v${finalAttrs.version}/SFM-${finalAttrs.version}-universal.dmg";
-      hash = "sha256-gb/U7ap0UJJuRHf6uVmOquWWWbVOgV6g/2swzrb16hs=";
+      hash = "sha256-3Wk0B0fvN4rcsjZQ2EOk+E4Ks7qccOMJLsJrAn8FdDM=";
       stripRoot = false;
       nativeBuildInputs = [ undmg ];
     };
