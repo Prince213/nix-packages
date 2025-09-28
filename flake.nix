65178d7bedda1363d74c4ef1c2dd750011a2795f
Sizhe Zhao <prc.zhao@outlook.com>
2025-09-28 13:18:20 +0800

    flake: use packagesFromDirectoryRecursive

--- flake.nix
+++ flake.nix
@@ -29,17 +29,12 @@
       flake = {
         nixosModules.default = ./modules;
         modulePackages.ngbe = ./pkgs/ngbe;
-        overlays.default = self: super: {
-          kodama = super.callPackage ./pkgs/kodama { };
-          linglong = super.callPackage ./pkgs/linglong { };
-          mccgdi = super.callPackage ./pkgs/mccgdi { };
-          nodebb = super.callPackage ./pkgs/nodebb { };
-          sing-box-app = super.callPackage ./pkgs/sing-box-app { };
-          sing-box-app-beta = super.callPackage ./pkgs/sing-box-app-beta { };
-          sing-box-beta = super.callPackage ./pkgs/sing-box-beta { };
-          vips_8_14_5 = super.callPackage ./pkgs/vips_8_14_5 { };
-          wubi98-fonts = super.callPackage ./pkgs/wubi98-fonts { };
-        };
+        overlays.default =
+          _: super:
+          super.lib.packagesFromDirectoryRecursive {
+            inherit (super) callPackage;
+            directory = ./pkgs;
+          };
       };
       perSystem =
         { system, pkgs, ... }:
