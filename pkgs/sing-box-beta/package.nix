4360438ecc8035a028ea773d23295236e190c345
Sizhe Zhao <prc.zhao@outlook.com>
2025-10-07 14:58:34 +0800

    pkgs/sing-box-beta: 1.13.0-alpha.8 -> 1.13.0-alpha.20

--- pkgs/sing-box-beta/package.nix
+++ pkgs/sing-box-beta/package.nix
@@ -4,11 +4,11 @@
 
 sing-box.overrideAttrs (previousAttrs: {
   pname = previousAttrs.pname + "-beta";
-  version = "1.13.0-alpha.8";
+  version = "1.13.0-alpha.20";
 
   src = previousAttrs.src.override {
-    hash = "sha256-4LtLeh+1fiCAwgHZK48YMHJOivFj3OGJUpWdlpK7PFE=";
+    hash = "sha256-klB0i0qg4lkL/BrgcZQ6R8QEOs1LST6bmjDFOn9j88E=";
   };
 
-  vendorHash = "sha256-vzjM7jOiOGyY+ucDABzWWo5a+W1+msnHsdMxHi7yb1A=";
+  vendorHash = "sha256-MBaxyfEWShRB1w79SMNMLz2WOR4fBbIpfufctMQRF9Q=";
 })
