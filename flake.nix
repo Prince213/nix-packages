68eaa024f92af65a8a94994d044a4c2b08c25753
Sizhe Zhao <prc.zhao@outlook.com>
2025-11-09 11:32:46 +0800

    flake: use nixpkgs/nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-compat = {
       url = "https://git.lix.systems/lix-project/flake-compat/archive/main.tar.gz";
       flake = false;
