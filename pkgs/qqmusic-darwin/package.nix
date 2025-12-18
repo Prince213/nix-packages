a5d90c8a31e55054161aac9472780f86853d541f
Sizhe Zhao <prc.zhao@outlook.com>
2025-12-18 13:55:52 +0800

    pkgs/qqmusic-darwin: 10.9.5.1 -> 10.9.6.2

--- pkgs/qqmusic-darwin/package.nix
+++ pkgs/qqmusic-darwin/package.nix
@@ -8,12 +8,12 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   inherit (qqmusic) pname;
-  version = "10.9.5.1";
+  version = "10.9.6.2";
 
   src = fetchurl rec {
-    name = "QQMusicMac10.9.5Build01.dmg";
-    url = "https://web.archive.org/web/20251123072458/https://dldir.y.qq.com/ecosfile/music_clntupate/mac/other/${name}?sign=1763882683-soEnopxjxa9lWaaJ-0-990abb13bf4511950e235ac28082b1bf";
-    hash = "sha256-gI4kAWOdI1t/0uSXmxQubVAqpF9mwTIAym6CqFAeaoU=";
+    name = "QQMusicMac10.9.6Build02.dmg";
+    url = "https://web.archive.org/web/20251218055730if_/https://dldir.y.qq.com/ecosfile/music_clntupate/mac/other/${name}?sign=1766037448-Ic5zxXL1l4JLZ321-0-a3bcd6b626a906f368d2568acddcfe04";
+    hash = "sha256-aDDmKuaHOjfFw7B/+pp4e2Li/NehHKBIm5PGkRpF0uQ=";
   };
 
   nativeBuildInputs = [ undmg ];
