012b4459b72d3bd2d8a8e9ddcea78c876ab8a63b
Sizhe Zhao <prc.zhao@outlook.com>
2025-12-26 00:12:13 +0800

    pkgs/sing-box-beta: 1.13.0-alpha.33 -> 1.13.0-alpha.34

--- pkgs/sing-box-beta/package.nix
+++ pkgs/sing-box-beta/package.nix
@@ -4,11 +4,11 @@
 
 sing-box.overrideAttrs (previousAttrs: {
   pname = previousAttrs.pname + "-beta";
-  version = "1.13.0-alpha.33";
+  version = "1.13.0-alpha.34";
 
   src = previousAttrs.src.override {
-    hash = "sha256-7LSBo2ZbzB0GsXJtIl8L1aCbrBGQg0UL7+yjiw0/KCc=";
+    hash = "sha256-R74Eio+o21zbTPvmROCduiK4ZQKBiv6OPoBsxLOQUqE=";
   };
 
-  vendorHash = "sha256-8PheEP6nEs1dMCCzA+UtzCbtuyAaWhrrv+zgh9Pse44=";
+  vendorHash = "sha256-TiuVCkrB+Op68HVzqhd1Bhwtc1lKo1swyx+RblAR/os=";
 })
