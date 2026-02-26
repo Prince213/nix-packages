4af3bd791c9010e6053337af84f4125f3f25aa2d
Sizhe Zhao <prc.zhao@outlook.com>
2026-02-27 05:48:21 +0800

    pkgs/sing-box-beta: clean up

--- pkgs/sing-box-beta/package.nix
+++ pkgs/sing-box-beta/package.nix
@@ -8,42 +8,38 @@
   withNaiveOutbound ? false,
 }:
 
-sing-box.overrideAttrs (
-  previousAttrs:
-  {
-    pname = previousAttrs.pname + "-beta";
-    version = "1.13.0-rc.6";
+sing-box.overrideAttrs (previousAttrs: {
+  pname = previousAttrs.pname + "-beta";
+  version = "1.13.0-rc.6";
 
-    src = previousAttrs.src.override {
-      hash = "sha256-yNZGUiNZh7fyW/BFgXcZg4ttnldRIDkB2KJ/MK5NH5E=";
-    };
+  src = previousAttrs.src.override {
+    hash = "sha256-yNZGUiNZh7fyW/BFgXcZg4ttnldRIDkB2KJ/MK5NH5E=";
+  };
 
-    vendorHash = "sha256-wBOu2Zac/PpUYKOxA5M56cyKdCLG2dQkBagKaGD8r4w=";
-  }
-  // lib.optionalAttrs (withNaiveOutbound && stdenvNoCC.hostPlatform.isDarwin) {
-    buildInputs = (previousAttrs.buildInputs or [ ]) ++ [ cronet-go ];
-    tags = previousAttrs.tags ++ lib.optional withNaiveOutbound "with_naive_outbound";
-  }
-  // lib.optionalAttrs (withNaiveOutbound && stdenvNoCC.hostPlatform.isLinux) {
-    modPostBuild = (previousAttrs.modPostBuild or "") + ''
-      cd vendor/github.com/sagernet/cronet-go
-      patch -p1 < ${./cronet-go.patch}
-      cd ../../../..
+  vendorHash = "sha256-wBOu2Zac/PpUYKOxA5M56cyKdCLG2dQkBagKaGD8r4w=";
+
+  preBuild = (previousAttrs.preBuild or "") + ''
+    cd vendor/github.com/sagernet/cronet-go
+    chmod -R u+w .
+    cp -r ${cronet-go}/ .
+    ${lib.optionalString (
+      withNaiveOutbound && stdenvNoCC.hostPlatform.isLinux
+    ) "patch -p1 < ${./cronet-go.patch}"}
+    cd ../../../..
+  '';
+
+  buildInputs =
+    (previousAttrs.buildInputs or [ ])
+    ++ lib.optional (withNaiveOutbound && stdenvNoCC.hostPlatform.isDarwin) cronet-go;
+
+  tags =
+    previousAttrs.tags
+    ++ lib.optional withNaiveOutbound "with_naive_outbound"
+    ++ lib.optional (withNaiveOutbound && stdenvNoCC.hostPlatform.isLinux) "with_purego";
+
+  postInstall =
+    previousAttrs.postInstall
+    + lib.optionalString (withNaiveOutbound && stdenvNoCC.hostPlatform.isLinux) ''
+      ln -s "${cronet-go}/lib/${go.GOOS}_${go.GOARCH}/libcronet.so" "$out/lib/"
     '';
-
-    vendorHash = "sha256-cxsUOMBuRFViUSQiDd2gQ7nzk+g4B908NTsAr4x7jtk=";
-
-    tags =
-      previousAttrs.tags
-      ++ lib.optionals withNaiveOutbound [
-        "with_purego"
-        "with_naive_outbound"
-      ];
-
-    postInstall =
-      previousAttrs.postInstall
-      + lib.optionalString withNaiveOutbound ''
-        ln -s "${cronet-go}/lib/${go.GOOS}_${go.GOARCH}/libcronet.so" "$out/lib/"
-      '';
-  }
-)
+})
