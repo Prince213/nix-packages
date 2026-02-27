74d095ce47d512738434d216f1a3ea52c2703a9b
Sizhe Zhao <prc.zhao@outlook.com>
2026-02-27 16:17:32 +0800

    pkgs/sing-box-beta: support static cronet for linux

--- pkgs/sing-box-beta/package.nix
+++ pkgs/sing-box-beta/package.nix
@@ -1,13 +1,15 @@
 {
+  buildPackages,
   cronet-go,
   go,
   lib,
   sing-box,
   stdenvNoCC,
 
+  withStaticCronet ? true,
   withNaiveOutbound ? false,
 }:
-
+assert withNaiveOutbound -> !withStaticCronet -> stdenvNoCC.hostPlatform.isLinux;
 sing-box.overrideAttrs (previousAttrs: {
   pname = previousAttrs.pname + "-beta";
   version = "1.13.0-rc.6";
@@ -18,28 +20,39 @@ sing-box.overrideAttrs (previousAttrs: {
 
   vendorHash = "sha256-wBOu2Zac/PpUYKOxA5M56cyKdCLG2dQkBagKaGD8r4w=";
 
-  preBuild = (previousAttrs.preBuild or "") + ''
-    cd vendor/github.com/sagernet/cronet-go
-    chmod -R u+w .
-    cp -r ${cronet-go}/ .
-    ${lib.optionalString (
-      withNaiveOutbound && stdenvNoCC.hostPlatform.isLinux
-    ) "patch -p1 < ${./cronet-go.patch}"}
-    cd ../../../..
-  '';
+  preBuild =
+    (previousAttrs.preBuild or "")
+    + lib.optionalString withNaiveOutbound ''
+      cd vendor/github.com/sagernet/cronet-go
+      chmod -R u+w .
+      cp -r ${cronet-go}/ .
+      ${lib.optionalString (!withStaticCronet) "patch -p1 < ${./cronet-go.patch}"}
+      cd ../../../..
+    '';
+
+  nativeBuildInputs =
+    previousAttrs.nativeBuildInputs
+    ++ lib.optional (withNaiveOutbound && withStaticCronet) buildPackages.rustc.llvmPackages.bintools;
 
   buildInputs =
     (previousAttrs.buildInputs or [ ])
-    ++ lib.optional (withNaiveOutbound && stdenvNoCC.hostPlatform.isDarwin) cronet-go;
+    ++ lib.optional (withNaiveOutbound && withStaticCronet) cronet-go;
 
   tags =
     previousAttrs.tags
-    ++ lib.optional withNaiveOutbound "with_naive_outbound"
-    ++ lib.optional (withNaiveOutbound && stdenvNoCC.hostPlatform.isLinux) "with_purego";
+    ++ lib.optionals withNaiveOutbound (
+      [ "with_naive_outbound" ] ++ lib.optional (!withStaticCronet) "with_purego"
+    );
 
   postInstall =
     previousAttrs.postInstall
-    + lib.optionalString (withNaiveOutbound && stdenvNoCC.hostPlatform.isLinux) ''
+    + lib.optionalString (withNaiveOutbound && !withStaticCronet) ''
       ln -s "${cronet-go}/lib/${go.GOOS}_${go.GOARCH}/libcronet.so" "$out/lib/"
     '';
+
+  env =
+    previousAttrs.env
+    // lib.optionalAttrs (withNaiveOutbound && withStaticCronet) {
+      CGO_LDFLAGS = "-fuse-ld=lld";
+    };
 })
