c78a3b0ed3920130ffca32edbeb6889ec0edae27
Sizhe Zhao <prc.zhao@outlook.com>
2026-03-15 21:55:56 +0800

    pkgs/sing-box-app: 1.13.2 -> 1.13.3

--- pkgs/sing-box-app/package.nix
+++ pkgs/sing-box-app/package.nix
@@ -10,11 +10,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "sing-box-app";
-  version = "1.13.2";
+  version = "1.13.3";
 
   src = fetchurl {
     url = "https://github.com/SagerNet/sing-box/releases/download/v${finalAttrs.version}/SFM-${finalAttrs.version}-Universal.pkg";
-    hash = "sha256-o9ovkgCMtjPwuEuXLJlh81Ur7OCpr12T3n6t5ZtLm+M=";
+    hash = "sha256-0Yxe2ltx2ex3/fX68P7dXpY31Do5f9kAp47iEK38WfM=";
   };
 
   nativeBuildInputs = [
