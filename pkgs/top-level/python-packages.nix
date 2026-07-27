3682aec819aea2f6f6c84697142ee11031f1e70f
Sizhe Zhao <prc.zhao@outlook.com>
2026-07-27 21:12:35 +0800

    python3Packages.mush: init at 2.8.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2,4 +2,5 @@ final: _: {
   adaptix = final.callPackage ../development/python-modules/adaptix { };
   durations = final.callPackage ../development/python-modules/durations { };
   fnv1a = final.callPackage ../development/python-modules/fnv1a { };
+  mush = final.callPackage ../development/python-modules/mush { };
 }
