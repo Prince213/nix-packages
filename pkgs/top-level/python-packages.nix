4497682a6effdbe00028a4d4c5f569e4f6240868
Sizhe Zhao <prc.zhao@outlook.com>
2026-07-27 20:56:13 +0800

    python3Packages.fnv1a: init at 0.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1,4 +1,5 @@
 final: _: {
   adaptix = final.callPackage ../development/python-modules/adaptix { };
   durations = final.callPackage ../development/python-modules/durations { };
+  fnv1a = final.callPackage ../development/python-modules/fnv1a { };
 }
