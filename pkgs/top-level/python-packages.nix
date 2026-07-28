2e365766c90fea23c295b9d9f693a62430fbce4e
Sizhe Zhao <prc.zhao@outlook.com>
2026-07-28 20:46:55 +0800

    python3Packages.pytest-structlog: init at 1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3,4 +3,5 @@ final: _: {
   durations = final.callPackage ../development/python-modules/durations { };
   fnv1a = final.callPackage ../development/python-modules/fnv1a { };
   mush = final.callPackage ../development/python-modules/mush { };
+  pytest-structlog = final.callPackage ../development/python-modules/pytest-structlog { };
 }
