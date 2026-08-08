6dcb8747d082b3a2bde03f945a2ca3b2656d841b
Sizhe Zhao <prc.zhao@outlook.com>
2026-08-08 12:08:35 +0800

    sing-box-beta: 1.14.0-beta.7 -> 1.14.0-beta.9

--- pkgs/by-name/si/sing-box-beta/package.nix
+++ pkgs/by-name/si/sing-box-beta/package.nix
@@ -4,14 +4,14 @@
 
 sing-box.overrideAttrs (previousAttrs: {
   pname = previousAttrs.pname + "-beta";
-  version = "1.14.0-beta.7";
+  version = "1.14.0-beta.9";
   __structuredAttrs = true;
 
   src = previousAttrs.src.override {
-    hash = "sha256-wLjezKTIVLbXsTgHQyaPyrOmoG8iPTKpapjHT7W1z+I=";
+    hash = "sha256-qUP0r6TQnb/ae+gfIwdnUtbWiYi9sNdK3Prh/qC3wT4=";
   };
 
-  vendorHash = "sha256-QDRLNatY0PHhM1GGusK/SOlCAK1le9Bf3t3Ns8rPG0Q=";
+  vendorHash = "sha256-Oxcl4YvPEQFhsljUFqQ9GJRkOz6OHlfuN3tA7eLeB8Y=";
 
   tags = previousAttrs.tags ++ [
     "with_cloudflared"
